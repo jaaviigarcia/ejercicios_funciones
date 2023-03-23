ada1bc814d4efc66abeee2bc0bcf1ec2460ccce1
jaaviigarcia <117916599+jaaviigarcia@users.noreply.github.com>
2023-03-23 14:02:34 +0100

    Add files via upload

---
+++ producto.js
@@ -0,0 +1,5 @@
+function producto(x1,x2){
+return x1*x2;
+}
+const resultado = producto (4,8)
+console.log("La multiplicación de 4 y 8 es:",resultado);
---
+++ suma.js
@@ -0,0 +1,5 @@
+function suma(a,b){
+return a + b;
+}
+const resultado = suma (4,8)
+console.log("La suma de 4 y 8 es:",resultado);
